79aeb6b44c8878f2423c4419759ba655f9e8a590
MiitcherONLaptop <mikael.gron@hotmail.com>
2017-07-19 21:22:47 +0300

    Added test.

--- test.py
+++ test.py
@@ -12,6 +12,9 @@ class Test_scraping(unittest.TestCase):
     def test_scrape_company2(self):
         self.assertEqual(3,3)
 
+    def test_scrape_company4(self):
+        self.assertEqual(3,3)
+
 
 class Test_scrape_KL(unittest.TestCase):
 
