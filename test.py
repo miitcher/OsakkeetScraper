edd257b757aff28497eed33da1437f9872bf10c8
MiitcherONLaptop <mikael.gron@hotmail.com>
2017-07-19 21:19:56 +0300

    Changed structure of testfiles.

---
+++ test.py
@@ -0,0 +1,23 @@
+"""
+cd /c/Eclipse\ workspace/OsakkeetScraper
+python -m unittest -v
+"""
+import unittest
+from scraping import *
+from scrape_KL import *
+
+
+class Test_scraping(unittest.TestCase):
+
+    def test_scrape_company2(self):
+        self.assertEqual(3,3)
+
+
+class Test_scrape_KL(unittest.TestCase):
+
+    def test_scrape_KL(self):
+        self.assertEqual(3,3)
+
+
+if __name__ == '__main__':
+    unittest.main()
