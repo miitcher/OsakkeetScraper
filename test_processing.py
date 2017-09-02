3d87c687465638eabfdb0f7958182955fca6cb6c
MikaelONWindows <mikael.gron@hotmail.com>
2017-09-02 09:57:24 +0300

    Worked on unittests for processing.Processor class.

--- test_processing.py
+++ test_processing.py
@@ -1,4 +1,4 @@
-import unittest
+import unittest, re
 import scrape_logger
 import storage
 
@@ -14,6 +14,7 @@ level = "WARNING"
 logger = scrape_logger.setup_logger(level)
 
 
+filename_all = "test\\metrics_test_all.json"
 test_filenames = [
     "test\\metrics_test_2048.json",
     "test\\metrics_test_2051.json"
@@ -23,12 +24,13 @@ test_expected_tulostiedot_key = [
     "2017-01-01"
 ]
 
+date = re.compile("^\d{4}\-\d{2}\-\d{2}$") # YYYY-MM-DD
+
 
 class Test(unittest.TestCase):
 
     def test_Processing_smoketest(self):
-        filename = "test\\metrics_test_all.json"
-        metrics_list = storage.load_metrics(filename)
+        metrics_list = storage.load_metrics(filename_all)
         for metrics in metrics_list:
             processor = processing.Processor(metrics)
             collection = processor.process()
@@ -45,12 +47,17 @@ class Test(unittest.TestCase):
             i += 1
 
     def test_collect_and_calculate_metrics(self):
-        for filename in test_filenames:
-            test_collect_and_calculate_metrics_Controll(self, filename)
+        metrics_list = storage.load_metrics(filename_all)
+        for metrics in metrics_list:
+            #print(json.dumps(metrics, indent=3))
+            test_collect_and_calculate_metrics_Controll(self, metrics)
 
     def test_filter(self):
-        for filename in test_filenames:
-            test_filter_Controll(self, filename)
+        #filename_all = "test\\metrics_test_2048.json"
+        metrics_list = storage.load_metrics(filename_all)
+        for metrics in metrics_list:
+            #print(json.dumps(metrics, indent=3))
+            test_filter_Controll(self, metrics)
 
 def test_get_tulostiedot_key_Controll(tester, filename, expected_date_str):
     metrics = storage.load_metrics(filename)[0]
@@ -58,27 +65,86 @@ def test_get_tulostiedot_key_Controll(tester, filename, expected_date_str):
     tulostiedot_key = processor.get_tulostiedot_key()
     tester.assertEqual(tulostiedot_key, expected_date_str)
 
-def test_collect_and_calculate_metrics_Controll(tester, filename):
-    metrics = storage.load_metrics(filename)[0]
-    #print(json.dumps(metrics, indent=3))
+def test_collect_and_calculate_metrics_Controll(tester, metrics):
+    type_dict = {
+        "tulostiedot_key": date,
+        "osinko_tuotto_%": dict,
+        "osinko_euro": dict,
+        "steady_osinko": bool,
+        "company_id": int,
+        "company_name": str,
+        "kurssi": float,
+        "kuvaus": str,
+        "scrape_date": date,
+        "toimiala": str,
+        "toimialaluokka": str,
+        "osakkeet_kpl": int,
+        "osinko_tuotto_%_fresh": float,
+        "P_fresh": float,
+        "needs_tulostiedot_key": dict
+    }
+    can_be_None = {
+        "tulostiedot_key",
+        "needs_tulostiedot_key",
+        "toimiala",
+        "toimialaluokka"
+    }
+    # Items in dicts are floats.
 
     processor = processing.Processor(metrics)
     collection = processor.collect_and_calculate_metrics()
+    #logger.error(json.dumps(collection, indent=3))
 
-    #print(json.dumps(collection, indent=3))
     tester.assertEqual(len(collection), 15)
-    # TODO: add more asserting
+    for key in collection:
+        if collection[key] is not None:
+            if type_dict[key] == date:
+                tester.assertTrue(date.match(collection[key]))
+            elif type_dict[key] == dict:
+                if key == "needs_tulostiedot_key" and collection[key]:
+                    if collection[key]["P"]:
+                        tester.assertEqual(len(collection[key]), 11)
+                    else:
+                        tester.assertEqual(len(collection[key]), 8)
+                for k, v in collection[key].items():
+                    if v is not None:
+                        tester.assertTrue(
+                            isinstance(v, float)
+                            or isinstance(v, int)
+                        )
+                    elif key != "needs_tulostiedot_key":
+                        logger.error(json.dumps(collection, indent=3))
+                        raise AssertionError("{} can not be None".format(k))
+            else:
+                tester.assertIsInstance(collection[key], type_dict[key])
+        elif key not in can_be_None:
+            logger.error(json.dumps(collection, indent=3))
+            raise AssertionError("{} can not be None".format(key))
 
-def test_filter_Controll(tester, filename):
-    metrics = storage.load_metrics(filename)[0]
-    #print(json.dumps(metrics, indent=3))
+def test_filter_Controll(tester, metrics):
+    # skipped_filters can be a list or None.
+    # The rest of the items in passed_filter are booleans.
 
     processor = processing.Processor(metrics)
     collection = processor.process()
+    #logger.error(json.dumps(collection["passed_filter"], indent=3))
 
-    #print(json.dumps(collection, indent=3))
     tester.assertEqual(len(collection), 16)
-    # TODO: add more asserting
+    passed_filter = collection["passed_filter"]
+    keys_list = []
+    for key in passed_filter:
+        keys_list.append(key)
+        if key == "skipped_filters":
+            skipped_filters = passed_filter["skipped_filters"]
+            if skipped_filters is not None:
+                tester.assertIsInstance(skipped_filters, list)
+                for v in skipped_filters:
+                    keys_list.append(v)
+                    tester.assertIsInstance(v, str)
+        else:
+            tester.assertIsInstance(passed_filter[key], bool)
+    tester.assertEqual(len(keys_list), 7)
+
 
 if __name__ == '__main__':
     unittest.main()
